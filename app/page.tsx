c05d2ff022d366a499dc6baf618aefebeb92fff2
unknown <viniciusreis3105@gmail.com>
2024-06-14 22:28:26 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/CategoryList";
 import Header from "./_components/Header";
 import SearchInput from "./_components/SearchInput";
@@ -5,12 +6,26 @@ import SearchInput from "./_components/SearchInput";
 export default function Home() {
   return (
     <>
+      {/* Componente Header */}
       <Header />
-      <div className="px-5 pt-6">
+
+      <div className="flex flex-col gap-6 px-5">
+        {/* Componente Input de Pesquisa */}
         <SearchInput />
-      </div>
-      <div className="px-5 pt-6">
+
+        {/* Componente Lista de Categorias */}
         <CategoryList />
+
+        {/* Banner Indicando promoções */}
+        <Image
+          src="/banner.png"
+          alt="Promo Banner"
+          width={0}
+          height={0}
+          quality={100}
+          className="h-auto w-full rounded-2xl"
+          sizes="100vw"
+        />
       </div>
     </>
   );
