a5addf90e0730fdd6f16b63303202843e1b5212b
unknown <viniciusreis3105@gmail.com>
2024-06-17 20:10:10 -0300

    chore: updating product detail component

--- app/products/[id]/_components/ProductDetail.tsx
+++ app/products/[id]/_components/ProductDetail.tsx
@@ -1,15 +1,30 @@
+"use client";
+
 import DiscountBadge from "@/app/_components/DiscountBadge";
+import { Button } from "@/app/_components/ui/button";
 import {
   calculateProductTotalPrice,
   formatCurrency,
 } from "@/app/_helpers/price";
 import { Product } from "@prisma/client";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import { useState } from "react";
 
 interface ProductDetailProps {
   product: Product;
 }
 
 export default function ProductDetail({ product }: ProductDetailProps) {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleIncreaseQuantityClick = () =>
+    setQuantity((currentState) => currentState + 1);
+  const handleDecreaseQuantityClick = () =>
+    setQuantity((currentState) => {
+      if (currentState === 1) return 1;
+
+      return currentState - 1;
+    });
   return (
     <div className="p-5">
       <h1 className="mb-3 text-xl font-medium">{product.name}</h1>
@@ -21,10 +36,33 @@ export default function ProductDetail({ product }: ProductDetailProps) {
             </h2>
             {product.discountPercentage && <DiscountBadge product={product} />}
           </div>
-          <span className="text-sm text-muted-foreground">
+          <p className="text-sm text-muted-foreground">
             De: {formatCurrency(Number(product.price))}
-          </span>
+          </p>
         </div>
+        <div className="flex items-center gap-2 text-center">
+          <Button
+            size="icon"
+            variant="ghost"
+            className="rounded-full border border-solid border-muted-foreground"
+            onClick={handleDecreaseQuantityClick}
+          >
+            <ChevronLeftIcon />
+          </Button>
+          <span className="w-4">{quantity}</span>
+          <Button
+            size="icon"
+            className="rounded-full"
+            onClick={handleIncreaseQuantityClick}
+          >
+            <ChevronRightIcon />
+          </Button>
+        </div>
+      </div>
+
+      <div className="mt-6 space-y-3">
+        <h1 className="font-semibold">Sobre</h1>
+        <p className="text-sm text-muted-foreground">{product.description}</p>
       </div>
     </div>
   );
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -38,7 +38,7 @@ async function main() {
       imageURL:
         "https://utfs.io/f/90074912-6eed-44ae-b142-25954f651883-hdxnba.png",
       price: 120,
-      discountPercentage: 0,
+      discountPercentage: 10,
       categoryId: "1",
     },
     {
@@ -47,16 +47,17 @@ async function main() {
       imageURL:
         "https://utfs.io/f/b95ca8ca-8a05-43fc-9e8f-88ec37c2acbb-hdxnb8.png",
       price: 225,
-      discountPercentage: 0,
+      discountPercentage: 5,
       categoryId: "2",
     },
     {
       name: "Mouse Gamer Razer",
-      description: "Mouse ergonômico com sensor óptico. Conexão sem fio",
+      description:
+        "Design ergonômico premiado: Confiado por mais  de 13 milhões de fãs em todo o mundo, o formato icônico da DeathAdder  forneceu vitórias para inúmeros profissionais mais do que qualquer outro  mouse, incluindo o tricampeão mundial de League of Legends, Faker",
       imageURL:
         "https://utfs.io/f/0292521a-3a1c-4db8-9ffd-2bae833339e5-hdxnb9.png",
       price: 120,
-      discountPercentage: 0,
+      discountPercentage: 15,
       categoryId: "3",
     },
     {
